06feabd3442e73c4f3fdeaef52c9f290fde181c9
1789719643@qq.com <1789719643@qq.com>
2021-03-14 19:25:39 +0800

    add some file

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,10 @@ const { resolve } = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/",
-  publicDir: "/",
+  base: "./",
+  build:{
+    outDir:"static_src"
+  },
   server: {
     port: '9999',
     open: true,
